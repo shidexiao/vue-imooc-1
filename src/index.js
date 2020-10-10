0415dfb08f601aa1db5a6a6198e58e796335b60c
shidexiao <sswsdx@163.com>
2020-10-10 19:58:37 +0800

    feat: webpack-dev-server test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import App from './app.vue';
+
+import './assets/styles/test.css';
+import './assets/styles/style.styl'
+import './assets/images/a.jpg';
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+new Vue({
+  render: (h) => h(App)
+}).$mount(root)
+
+if (module.hot) {
+  console.log('hot....');
+}
